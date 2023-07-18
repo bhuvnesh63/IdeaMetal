d1e43ab0054a44ea58e0dbbc8a160a8841f8b451
pooja092 <121798317+pooja092@users.noreply.github.com>
2023-07-18 13:56:12 +0530

    create a category module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,29 @@ import Dashboard from './components/Admin/Dashboard/Dashboard'
 import Category from './components/Admin/Category/Category'
 import Material from './components/Admin/Material/Material'
 import Signup from './components/Admin/Signup/Signup'
+import CategoryList from './components/Admin/Category/CategoryList'
+
+
 
 const App = () => {
+
   return (
     <>
+
+
     {/* <Header /> */}
+
+    
     <Routes>
     <Route path="/dashboard" element={<Sidebar><Dashboard/> </Sidebar>}/>
     <Route path="/items" element={<Sidebar><Item/></Sidebar> } />
     <Route path="/category" element={<Sidebar><Category/></Sidebar>}/>
+    <Route path="/category-list" element={<Sidebar><CategoryList/></Sidebar>}/>
     <Route path="/material" element={<Sidebar><Material/></Sidebar>}/>
     <Route path="/signup" element={<Sidebar><Signup/></Sidebar>}/>
     </Routes>
+
+
     </>
 
   )
